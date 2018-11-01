064dbd183bb233c669f58e3683faaa2275775e50
xujiam <1099213876@qq.com>
2018-11-01 11:07:54 +0800

    更新workpress

--- README.md
+++
@@ -1,1 +0,0 @@
-# xujm-blog
--- wp-content/themes/blog/header.php
+++ wp-content/themes/blog/header.php
@@ -6,6 +6,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
 <meta http-equiv="Cache-Control" content="no-transform">
 <meta http-equiv="Cache-Control" content="no-siteapp">
+<link rel="shortcut icon" href="favicon.ico">
 <title><?php 
 	$t = trim(wp_title('', false)); 
 	if($t) echo $t._hui('connector'); 
